f8e3817077df3dc13bf3f31a828e07b58e62168f
Andrius <andrius.rochalazarino@gmail.com>
2020-03-09 11:54:32 -0400

    Commit inicial corrigido

--- futureninjas/src/App.js
+++ futureninjas/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
+import { MuiThemeProvider, createGenerateClassName, jssPreset } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { AppContainer } from './components/AppContainer'
+
+const generateClassName = createGenerateClassName()
+const jss = create({
+	...jssPreset(),
+	// We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+	insertionPoint: document.getElementById('jss-insertion-point'),
+})
+
+const theme = createMuiTheme()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<JssProvider jss={jss} generateClassName={generateClassName}>
+			<MuiThemeProvider theme={theme}>
+				<CssBaseline />
+				<AppContainer />
+			</MuiThemeProvider>
+		</JssProvider>
+	)
 }
 
-export default App;
+export default App
