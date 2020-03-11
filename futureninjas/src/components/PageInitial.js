b778e8a6b6b72fd49e18b686fc0495df8d412309
Andrius <andrius.rochalazarino@gmail.com>
2020-03-11 12:18:04 -0400

    Layout com menu quase pronto

---
+++ futureninjas/src/components/PageInitial.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Main = styled.div`
+
+`
+
+export class PageInitial extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (<Main>
+			Sou a página inicial
+		</Main>)
+	}
+}
